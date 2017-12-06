88502389a8760216aefe52520b36ce252ade6ff6
wanghongqing <272338444@qq.com>
2017-12-06 11:37:25 +0800

    添加 轮播图搞定
    已经可以轮播本地图片

--- QQScrollView/QQScrollView/Classes/Home/A/View/QQHomeAHeaderView.m
+++ QQScrollView/QQScrollView/Classes/Home/A/View/QQHomeAHeaderView.m
@@ -7,6 +7,7 @@
 //
 
 #import "QQHomeAHeaderView.h"
+#import <SDCycleScrollView.h>
 
 static CGFloat headerViewHeight = 180;
 
@@ -18,8 +19,21 @@ static CGFloat headerViewHeight = 180;
     if (self) {
         self.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, headerViewHeight);
         self.backgroundColor = [UIColor redColor];
+        [self setupUI];
     }
     return self;
+}
+
+#pragma mark - SetupUI
+- (void)setupUI {
+    
+    NSArray *array = @[
+                       @"qq_cycle_placeholder",
+                       @"qq_cycle_test"
+                       ];
+    CGRect frame = CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height);
+    SDCycleScrollView *cycleScrollView = [SDCycleScrollView cycleScrollViewWithFrame:frame imageNamesGroup:array];
+    [self addSubview:cycleScrollView];
 }
 
 @end
